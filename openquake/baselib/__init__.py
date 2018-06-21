706f4992989fcbd69a57ca0a007e1af0a871136a
Michele Simionato <michele.simionato@gmail.com>
2018-06-21 06:33:07 +0200

    Cleanup [demos]

--- openquake/baselib/__init__.py
+++ openquake/baselib/__init__.py
@@ -102,9 +102,6 @@ def boolean(flag):
 
 config.read(soft_mem_limit=int, hard_mem_limit=int, port=int,
             multi_user=boolean)
-config.zworkers['master_host'] = config.general.master_host
-config.dbserver['host'] = config.general.master_host
-config.amqp['host'] = config.general.master_host
 config.general['precision'] = float(config.general.precision)
 
 if config.directory.custom_tmp:
